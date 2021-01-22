0fffa537cb8f13f51657b50464479d44a63cb653
TOSH-0001\usuario local <imorales@list.es>
2021-01-22 21:25:31 +0100

    Empty project

---
+++ src/test/java/ProductionTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+public class ProductionTest {
+
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+
+}
