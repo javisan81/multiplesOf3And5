107b4656c023fcfddc0fc2d09d5b3a94bec1914f
Javier Lopez <javisan81@hotmail.com>
2021-02-02 19:15:52 +0100

    lets triangle a little

--- src/test/java/SumOfMultiplesOf3And5.java
+++ src/test/java/SumOfMultiplesOf3And5.java
@@ -5,9 +5,16 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class SumOfMultiplesOf3And5 {
     private int sumNumbersMultipleOf3And5Below(int number) {
-        if(number == 3)
-            return 3;
-        return 0;
+        int sum = 0;
+        if (number == 3)
+            sum += 3;
+        if (number == 4)
+            sum += 3;
+        if (number == 5) {
+            sum += 3;
+            sum += 5;
+        }
+        return sum;
     }
 
     @Test
@@ -25,6 +32,14 @@ public class SumOfMultiplesOf3And5 {
         assertEquals(sumNumbersMultipleOf3And5Below(3), 3);
     }
 
+    @Test
+    public void Four() {
+        assertEquals(sumNumbersMultipleOf3And5Below(4), 3);
+    }
 
+    @Test
+    public void Five() {
+        assertEquals(sumNumbersMultipleOf3And5Below(5), 8);
+    }
 
 }
