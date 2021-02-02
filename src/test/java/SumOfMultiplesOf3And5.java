6e9f6124a5d4508f84f6c7bba95009cc9119b728
Javier Lopez <javisan81@hotmail.com>
2021-02-02 19:11:02 +0100

    zero and 1 created

---
+++ src/test/java/SumOfMultiplesOf3And5.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SumOfMultiplesOf3And5 {
+    private int sumNumbersMultipleOf3And5Below(int number) {
+        return 0;
+    }
+
+    @Test
+    public void Zero() {
+        assertEquals(sumNumbersMultipleOf3And5Below(0), 0);
+    }
+
+    @Test
+    public void One() {
+        assertEquals(sumNumbersMultipleOf3And5Below(1), 0);
+    }
+
+
+}
