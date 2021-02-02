94deb8182c26ec0ccdac88ec714ac928c70e660f
Javier Lopez <javisan81@hotmail.com>
2021-02-02 19:18:23 +0100

    putting some cases together

--- src/test/java/SumOfMultiplesOf3And5.java
+++ src/test/java/SumOfMultiplesOf3And5.java
@@ -6,12 +6,9 @@ import static org.junit.jupiter.api.Assertions.*;
 public class SumOfMultiplesOf3And5 {
     private int sumNumbersMultipleOf3And5Below(int number) {
         int sum = 0;
-        if (number == 3)
-            sum += 3;
-        if (number == 4)
+        if (number <= 5 && number>1)
             sum += 3;
         if (number == 5) {
-            sum += 3;
             sum += 5;
         }
         return sum;
